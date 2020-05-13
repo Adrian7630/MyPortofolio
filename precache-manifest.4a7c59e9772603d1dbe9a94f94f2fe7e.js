7acb5de75dac56701c0eefcf4e21ba969b549d72
Adrian7630 <businessman_32@yahoo.com>
2020-05-13 13:21:21 +0300

    Updates

---
+++ precache-manifest.4a7c59e9772603d1dbe9a94f94f2fe7e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8346028cb99e8ae118316582922c1206",
+    "url": "/MyPortofolio/index.html"
+  },
+  {
+    "revision": "f401e626a6975d0adf99",
+    "url": "/MyPortofolio/static/css/main.b4b2d39c.chunk.css"
+  },
+  {
+    "revision": "78f2cda7665d23fdcf7b",
+    "url": "/MyPortofolio/static/js/2.07b1f947.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MyPortofolio/static/js/2.07b1f947.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f401e626a6975d0adf99",
+    "url": "/MyPortofolio/static/js/main.82278653.chunk.js"
+  },
+  {
+    "revision": "f9e5ae7b9ff2803b21a8",
+    "url": "/MyPortofolio/static/js/runtime-main.3f71223e.js"
+  },
+  {
+    "revision": "a57b9149367f26d738a62509a4dd824c",
+    "url": "/MyPortofolio/static/media/avatar.a57b9149.jpg"
+  },
+  {
+    "revision": "e1dd04d69cafd730e198dc8004242b56",
+    "url": "/MyPortofolio/static/media/react.e1dd04d6.jpg"
+  },
+  {
+    "revision": "779613a982fa0f6f55da34db6b54196c",
+    "url": "/MyPortofolio/static/media/web.779613a9.jpg"
+  }
+]);
